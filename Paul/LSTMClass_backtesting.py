596d25f5e5fa54641b46ade75868b39605f48b66
Paul Heller <95037561+paulffm@users.noreply.github.com>
2022-12-31 12:40:28 +0100

    Update and rename LSTMCLass_backtesting.py to LSTMClass_backtesting.py

--- Paul/LSTMClass_backtesting.py
+++ Paul/LSTMClass_backtesting.py
@@ -25,33 +25,6 @@ from sklearn.metrics import accuracy_score, mean_squared_error, mean_absolute_er
 np.random.seed(10)
 
 
-# (Bidirectional(LSTM(20,
-
-def build_model(n_inputs, n_features):
-    model = Sequential()
-    model.add(LSTM(units=400, return_sequences=True, input_shape=(n_inputs, n_features)))
-    model.add(Dropout(0.1))
-    model.add(LSTM(units=400))
-    model.add(Dropout(0.1))
-    # model.add(Dense(32, kernel_initializer="uniform", activation='relu'))
-    model.add(Dense(units=1, activation='linear'))
-    model.compile(optimizer='adam', loss='mean_squared_error')
-    # history = model.fit
-    return model
-
-
-def Bi_LSTM(n_inputs, n_features):
-    optim = Adam(learning_rate=0.01)
-    model = Sequential()
-    model.add(Bidirectional(LSTM(units=800, return_sequences=True, input_shape=(n_inputs, n_features))))
-    model.add(Dropout(0.1))
-    model.add(Bidirectional(LSTM(units=800)))
-    model.add(Dropout(0.1))
-    # model.add(Dense(32, kernel_initializer="uniform", activation='relu'))
-    model.add(Dense(units=1, activation='linear'))
-    model.compile(optimizer=optim, loss='mean_squared_error')
-    # history = model.fit
-    return model
 
 def class_LSTM(n_inputs, n_features):
     model = Sequential()
@@ -64,18 +37,8 @@ def class_LSTM(n_inputs, n_features):
     return model
 
 
-def LSTM_HPO(n_inputs, n_features):
-    opt = Adamax(learning_rate=0.01)
-    nn = Sequential()
-    nn.add(Bidirectional(LSTM(units=175, return_sequences=True, input_shape=(n_inputs, n_features))))
-    nn.add(Dropout(0.01))
-    nn.add(Bidirectional(LSTM(units=175)))
-    nn.add(Dense(1, activation='linear'))
-    nn.compile(loss='mse', optimizer=opt, metrics=['mse', 'mae'])
-    return nn
 
-
-def prepare_data(data_set_scaled, backcandles, liste, splitlimit):
+def prepare_data(data_set_scaled, backcandles, liste, pred_days):
     X = []
     for j in range(len(liste)):  # data_set_scaled[0].size):#2 columns are target not X
         X.append([])
@@ -87,9 +50,12 @@ def prepare_data(data_set_scaled, backcandles, liste, splitlimit):
 
     X, yi = np.array(X), np.array(data_set_scaled[backcandles:, -1])
     y = np.reshape(yi, (len(yi), 1))
+    splitlimit = X.shape[0] - pred_days
 
     X_train, X_test = X[:splitlimit], X[splitlimit:]
     y_train, y_test = y[:splitlimit], y[splitlimit:]
+    print('shape', X_train)
+    print('shapes', y_train.shape, y_test.shape)
 
     return X_train, X_test, y_train, y_test
 
@@ -120,14 +86,11 @@ def main():
     # import data
     data = pd.read_csv('featureSelectionDataset_Paul.csv', sep=',', header=0, index_col=0, parse_dates=True,
                        decimal=".")
-    print(data)
     data = data.iloc[-600:,:]
     data.dropna(inplace=True)
     y_open = np.asarray([1 if data.Return_open[i]>0 else 0 for i in range(len(data))]).reshape(-1, 1)
     y_close = np.asarray([1 if data.Return_close[i]>0 else 0 for i in range(len(data))]).reshape(-1, 1)
     y_target = np.asarray([1 if data.Target[i]>0 else 0 for i in range(len(data))]).reshape(-1, 1)
-    print(y_target, y_target.shape)
-    print(y_open)
     data = data.drop(['Target'], axis=1)
     print(data)
 
@@ -137,22 +100,23 @@ def main():
     data_set_scaled = np.concatenate((data_set_scaled, y_open), axis=1)
     data_set_scaled = np.concatenate((data_set_scaled, y_close), axis=1)
     data_set_scaled = np.concatenate((data_set_scaled, y_target), axis=1)
-    print('data_scaled', data_set_scaled, data_set_scaled.shape)
+    print('data_scaled',data_set_scaled.shape)
     
 
     # choose how many look back days
-    backcandles = 50
+    backcandles = 30
 
     # choose columns: all but open price and target
     liste = list(range(0, data.shape[1] - 1))
-    print(data.iloc[:, liste])
+    
+    #print(data.iloc[:, liste])
 
     # split data into train test sets
-    splitlimit = (len(data) - 60)
+    pred_days = 50
 
     # prepare data for lstm
-    X_train, X_test, y_train, y_test = prepare_data(data_set_scaled, backcandles, liste, splitlimit)
-    print('test', y_test)
+    X_train, X_test, y_train, y_test = prepare_data(data_set_scaled, backcandles, liste, pred_days)
+    print('test', y_test.shape)
 
     # build model
 
@@ -178,13 +142,13 @@ def main():
 
     # predict
     y_pred = model.predict(X_test)
-    print('y_pred', y_pred)
+    
     
     y_pred[y_pred > 0.5] = 1
     y_pred[y_pred <= 0.5] = -1
     
     y_test[np.where(y_test == 0)] = -1
-    print(y_test)
+  
     
 
     # compare decisions
