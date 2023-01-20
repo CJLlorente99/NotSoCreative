5b6fe8f6778904d7fc61e9b9de18fec24e21e20b
CJLlorente99 <cj.llorente@alumnos.upm.es>
2023-01-20 08:27:45 +0100

    Change in backcandles and add investor with different decision rules

--- LSTM/investorLSTMWindowMinMaxScaler.py
+++ LSTM/investorLSTMWindowMinMaxScaler.py
@@ -127,7 +127,7 @@ class InvestorLSTMWindowMinMaxT2 (Investor):
 		step_out = 3
 
 		# how many last days i include in my prediction
-		backcandles = 20  # 10
+		backcandles = 16  # 10
 
 		# to save decision and predictions
 		y_predictions = []
@@ -286,7 +286,7 @@ class InvestorLSTMWindowMinMaxT1 (Investor):
 		step_out = 3
 
 		# how many last days i include in my prediction
-		backcandles = 20  # 10
+		backcandles = 16  # 10
 
 		# to save decision and predictions
 		y_predictions = []
@@ -328,6 +328,162 @@ class InvestorLSTMWindowMinMaxT1 (Investor):
 		# build decision rule: if open_t+2 > open_t -> buy on open_t
 		if res.Open.values[-1] < y_mean[1]:
 			return 1
+		else:
+			return -1
+
+class InvestorLSTMWindowMinMaxT1T2 (Investor):
+
+	def __init__(self, initialInvestment=10000, n_members=10):
+		super().__init__(initialInvestment)
+		self.n_members = n_members
+
+	def returnBrokerUpdate(self, moneyInvestedToday, data) -> pd.DataFrame:
+		return pd.DataFrame(
+			{'moneyToInvestLSTMWindowMinMaxT1T2': moneyInvestedToday,
+			 'investedMoneyLSTMWindowMinMaxT1T2': self.investedMoney,
+			 'nonInvestedMoneyLSTMWindowMinMaxT1T2': self.nonInvestedMoney}, index=[0])
+
+	def possiblyInvestMorning(self, data):
+		self.perToInvest = self.calculatePrediction(data['df'])
+
+	def possiblyInvestAfternoon(self, data):
+		global modelMinMaxScaler
+		self.perToInvest = 0
+		modelMinMaxScaler = [None, None, None, None, None]
+
+	def plotEvolution(self, expData, stockMarketData, recordPredictedValue=None):
+		"""
+				Function that plots the actual status of the investor investment as well as the decisions that have been made
+				:param indicatorData: Data belonging to the indicator used to take decisions
+				:param stockMarketData: df with the stock market data
+				:param recordPredictedValue: Predicted data dataframe
+				"""
+		# Plot indicating the evolution of the total value and contain (moneyInvested and moneyNotInvested)
+		fig = go.Figure()
+		fig.add_trace(
+			go.Scatter(name="Money Invested", x=self.record.index, y=self.record["moneyInvested"], stackgroup="one"))
+		fig.add_trace(go.Scatter(name="Money Not Invested", x=self.record.index, y=self.record["moneyNotInvested"],
+								 stackgroup="one"))
+		fig.add_trace(go.Scatter(name="Total Value", x=self.record.index, y=self.record["totalValue"]))
+		fig.update_layout(
+			title="Evolution of Porfolio using LSTM Window MM T1T2(" + self.record.index[0].strftime(
+				"%d/%m/%Y") + "-" +
+				  self.record.index[-1].strftime("%d/%m/%Y") + ")", xaxis_title="Date",
+			yaxis_title="Value [$]", hovermode='x unified')
+		fig.write_image("images/EvolutionPorfolioLSTMWindowMMT1T2(" + self.record.index[0].strftime(
+			"%d_%m_%Y") + "-" +
+						self.record.index[-1].strftime("%d_%m_%Y") + ").png", scale=6, width=1080, height=1080)
+		# fig.show()
+
+		# Plot indicating the value of the indicator, the value of the stock market and the decisions made
+		fig = make_subplots(rows=2, cols=1, specs=[[{"secondary_y": True}],
+												   [{"secondary_y": False}]])
+
+		fig.add_trace(go.Scatter(name="Stock Market Value Open", x=self.record.index,
+								 y=stockMarketData.Open[-len(self.record.index):]), row=1, col=1, secondary_y=False)
+		fig.add_trace(go.Bar(name="Money Invested Today", x=self.record.index, y=self.record["moneyInvestedToday"]),
+					  row=2, col=1, secondary_y=False)
+
+		fig.update_xaxes(title_text="Date", row=1, col=1)
+		fig.update_xaxes(title_text="Date", row=2, col=1)
+		fig.update_layout(
+			title="Decision making under LSTM Window MM T1T2(" + self.record.index[0].strftime("%d/%m/%Y") + "-" +
+				  self.record.index[-1].strftime("%d/%m/%Y") + ")", hovermode='x unified')
+		fig.write_image("images/DecisionMakingLSTMWindowMMT1T2(" + self.record.index[0].strftime("%d_%m_%Y") + "-" +
+						self.record.index[-1].strftime("%d_%m_%Y") + ").png", scale=6, width=1080, height=1080)
+
+	# fig.show()
+
+	def calculatePrediction(self, data):
+		# data is already shifted when it comes here
+		res = pd.DataFrame()
+		res['Open'] = data.Open
+		res['Close_t1'] = data.Close
+		res['Volume_t1'] = data.Volume
+		res['High_t1'] = data.High
+		res['Low_t1'] = data.Low
+		res['Diff_outra'] = res.Open - res.Close_t1
+		res['Return_outra'] = np.log(res.Open) - np.log(res.Close_t1)
+		res['Diff_intra'] = res.Close_t1 - res.Open.shift()
+		res['Return_intra'] = np.log(res.Close_t1) - np.log(res.Open.shift())
+		res['Diff_open'] = res.Open - res.Open.shift()
+		res.dropna(inplace=True)
+
+		# transformation
+		# to decide if we want to predict raw prices or returns -> i think returns might work better cause of non-stationarity
+		transformation = 1
+		if transformation == 0:
+			res['Target'] = res.Open
+
+		elif transformation == 1:
+
+			df_log = np.sqrt(np.log(res.Open))
+			res['Open_log'] = df_log
+			df_log_diff = df_log - df_log.shift()
+			res['Return_Open'] = df_log_diff
+			res['Target'] = df_log_diff
+			res.dropna(inplace=True)
+
+		# choose columns: all but target variable (its last column)
+		liste = list(range(0, data.shape[1]))
+
+		# only for
+		pred_days = 3
+
+		# how many days i want to predict
+		step_out = 3
+
+		# how many last days i include in my prediction
+		backcandles = 16  # 10
+
+		# to save decision and predictions
+		y_predictions = []
+
+		# days to predict
+		test_days = step_out
+
+		# scale data
+		scaler = MinMaxScaler()
+		data_set_scaled = scaler.fit_transform(res)
+
+		# prepare data for lstm
+		data_set_scaled = np.vstack([data_set_scaled, np.zeros((test_days, data_set_scaled.shape[1]))])
+		X_train, X_test, y_train, y_test = prepare_multidata(data_set_scaled, backcandles, pred_days, test_days)
+
+		n_members = self.n_members
+		epochs = 15 #
+		batch_size = 8
+		ensemble, y_pred_scale, = fit_ensemble(n_members, X_train, X_test, y_train, y_test, epochs,
+											   batch_size)
+
+		# inverse scaling
+		y_pred = inverse_scaling(res, y_pred_scale, scaler)
+
+		# bounds, mean -> further I only use mean
+		lower, y_mean, upper = calculate_bounds(y_pred)
+
+		# inverse transformation: not possible to use for real script -> because of index, in real script we have no test_or
+		# so for real script it might be better to use not a index or so
+		if transformation == 1:
+			y_mean = pd.DataFrame(y_mean, range(len(y_mean)), columns=['Open'])
+			y_mean['Open'] = y_mean['Open'] + df_log.shift().values[-test_days:]
+			y_mean = (y_mean ** 2)
+			y_mean = np.exp(y_mean)
+
+		y_predictions.append(np.asarray(y_mean))
+		y_mean = np.asarray(y_mean).flatten()
+
+		# build decision rule
+		if res.Open.values[-1] < y_mean[1] < y_mean[2]:
+			return 1
+		elif res.Open.values[-1] < y_mean[2] < y_mean[1]:
+			return 0.5
+		elif y_mean[1] < res.Open.values[-1] < y_mean[2]:
+			return -0.5
+		elif y_mean[2] < res.Open.values[-1] and y_mean[1] < res.Open.values[-1]:
+			return -1
+		elif y_mean[2] < res.Open.values[-1] < y_mean[1]:
+			return 0
 		else:
 			return -1
 
