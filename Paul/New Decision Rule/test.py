8172705b0016a6f829c16c40d14154c780d1dfee
Paul Heller <paulleonardoheller@yahoo.de>
2023-01-25 09:59:16 +0100

    t

--- Paul/New Decision Rule/test.py
+++ Paul/New Decision Rule/test.py
@@ -1,4 +1,19 @@
-print('hl')
+import numpy as np
 
-d = 1
-d=0
+print('hl')
+class A(object):
+ def __init__(self):
+   print("world")
+
+class B(A):
+    def __init__(self):
+        print("hello")
+        super().__init__()
+
+
+a = A()
+b=B()
+a = np.arange(0, 20)
+print(a)
+
+print(a[a > 10])
