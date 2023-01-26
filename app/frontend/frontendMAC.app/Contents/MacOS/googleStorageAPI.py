b184728e27aefeba744ce76c333a7eef5bc4d41f
CJLlorente99 <cj.llorente@alumnos.upm.es>
2023-01-27 00:13:53 +0100

    Trying to make MAC .app

---
+++ app/frontend/frontendMAC.app/Contents/MacOS/googleStorageAPI.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from io import StringIO
+from google.cloud.storage import Client
+import os
+import json
+
+url = 'https://storage.googleapis.com/datascienceii/myData.csv'
+projectName = 'datascienceii'
+bucketName = 'datascienceii'
+objectNameDf = 'myData.csv'
+credentials = './application_default_credentials.json'
+
+
+def readBlobDf():
+	os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credentials
+	storageClient = Client(project=projectName)
+	bucket = storageClient.bucket(bucketName)
+	blob = bucket.blob(objectNameDf)
+	try:
+		contents = StringIO(blob.download_as_string().decode('utf-8'))
+		df = pd.read_csv(contents)
+	except:
+		df = pd.DataFrame()
+	return df
