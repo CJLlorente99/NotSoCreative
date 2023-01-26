409bf09d4ac8851e1b8cdcacd96881a189269b32
CJLlorente99 <cj.llorente@alumnos.upm.es>
2023-01-26 11:54:30 +0100

    .

--- app/frontend/installPackage.py
+++ app/frontend/installPackage.py
@@ -2,10 +2,10 @@ import pip
 import sys
 
 def import_or_install(package):
-    try:
-        __import__(package)
-    except ImportError:
-        pip.main(['install', package])
+	try:
+		__import__(package)
+	except ImportError:
+		pip.main(['install', package])
 
 if __name__ == '__main__':
 	import_or_install(sys.argv[1])
