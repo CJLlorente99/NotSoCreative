4198be1a79557f37f094bd90e3576ac47e582ec4
CJLlorente99 <cj.llorente@alumnos.upm.es>
2023-01-29 22:46:21 +0100

    Render of short daily digest done. Example sending of email done.

--- app/dailyDigestAutomatization/main.py
+++ app/dailyDigestAutomatization/main.py
@@ -1,3 +1,10 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from renderer import Renderer
+import pytz
+from datetime import datetime
+
 """
 This file is to be called after the stock market is closed in order to send the daily digest
 1) Perform checks (has stock market close already, has stock market opened today)
@@ -6,9 +13,37 @@ This file is to be called after the stock market is closed in order to send the 
 4) Send emails
 """
 
+notSoCreativeEmail = 'notsocreative2023@gmail.com'
+destinationEmail = 'paulleonardo.heller@stud.tu-darmstadt.de'
 
 def main():
-	pass
+
+	"""
+	2) Instantiate a Renderer and render the two types of html strings (long and short)
+	"""
+	todayDate = datetime.now(pytz.timezone('America/New_York'))
+	rend = Renderer(todayDate)
+	renderedHTML = rend.renderShortDailyDigest()
+
+	"""
+	4) Send emails
+	"""
+	# Create a msg with the different parts
+	msg = MIMEMultipart('alternative')
+	msg['Subject'] = '[DataScienceII] Daily Digest ' + todayDate.strftime('%d/%m/%Y')
+	msg['From'] = notSoCreativeEmail
+	msg['To'] = destinationEmail
+	msg.attach(MIMEText(renderedHTML, 'html'))
+
+	# Connect with SMTP gmail server and send the email through it
+	s = smtplib.SMTP('smtp.gmail.com', 587)
+	s.ehlo()
+	s.starttls()
+	s.ehlo()
+	s.login(notSoCreativeEmail, 'nfkpbmnkifameswu')
+	s.sendmail(notSoCreativeEmail, destinationEmail, msg.as_string())
+	s.close()
+
 
 if __name__ == '__main__':
 	main()
--- app/dailyDigestAutomatization/renderer.py
+++ app/dailyDigestAutomatization/renderer.py
@@ -1,11 +1,20 @@
-# This file contains a class to be handled from main.py
+from datetime import timedelta, datetime
+import chart_studio.plotly as py
+import chart_studio.tools
 import pandas as pd
+import pytz
+import yfinance as yf
 from googleStorageAPI import readBlobDf
-import jinja2
+from jinja2 import Environment, FileSystemLoader
 import plotly.graph_objects as go
 
-shortDailyDigestTemplatePath = ''
-longDailyDigestTemplatePath = ''
+shortDailyDigestTemplatePath = 'shortDailyDigest.html'
+longDailyDigestTemplatePath = 'longDailyDigest.html'
+
+usernameChartStudio = 'NotSoCreative'
+apiChartStudio = 'c6a9xRk9hFTuEGqZ6xPT'
+
+chart_studio.tools.set_credentials_file(username=usernameChartStudio, api_key=apiChartStudio)
 
 """
 TODO
@@ -14,30 +23,64 @@ Plotly has support for creating html strings. Does it work by its own?
 """
 
 class Renderer:
-	def __init__(self):
+	def __init__(self, todayDate):
+		self.todayDate = todayDate
 		self.stockData = self.getYfData()
-		self.strategiesData = self.getStrategiesData()
+		self.strategiesData = self.getStrategiesData(['ca_25_1_2022', 'random_25_1_2022', 'idle_25_1_2022',
+													  'bah_25_1_2022', 'bilstmWindowRobMMT1T2Legacy_25_1_2023'])
+		self.env = Environment(loader=FileSystemLoader(searchpath='./htmlTemplates/'))
 
 	def getYfData(self) -> pd.DataFrame:
 		"""
 		This method should call yfinance and retrieve the data until today
 		:return: pd.DataFrame containing the stock market data
 		"""
-		return pd.DataFrame()
+		return yf.download('^GSPC', (self.todayDate-timedelta(10)).strftime('%Y-%m-%d'), self.todayDate.strftime('%Y-%m-%d'))
 
-	def getStrategiesData(self) -> pd.DataFrame:
+	def getStrategiesData(self, listStrategies) -> dict:
 		"""
 		This method should retrieve the strategies data from Google storage via GoogleStorageAPI
 		:return:
 		"""
-		return pd.DataFrame()
+		res = {}
+		df = readBlobDf()
+		for strategy in listStrategies:
+			res[strategy] = df.query('investorStrategy == @strategy')
+		return res
 
 	def renderShortDailyDigest(self) -> str:
 		"""
 		This method should render the short daily digest template
 		:return: html string
 		"""
-		return ''
+		todayDecision = 'hold'
+		if self.strategiesData['bilstmWindowRobMMT1T2Legacy_25_1_2023'].MoneyInvestedToday[-2] > 0:
+			todayDecision = 'buy'
+		elif self.strategiesData['bilstmWindowRobMMT1T2Legacy_25_1_2023'].MoneyInvestedToday[-2] < 0:
+			todayDecision = 'sell'
+
+		graph_url = self.generateHTMLImageMPV()
+
+		template = self.env.get_template(shortDailyDigestTemplatePath)
+		renderedHTML = template.render(name='Kim Erik',
+									   openPrice=self.stockData.Open[-1],
+									   closePrice=self.stockData.Close[-1],
+									   moneyInvestedToday=self.strategiesData['bilstmWindowRobMMT1T2Legacy_25_1_2023'].MoneyInvestedToday[-2],
+									   totalPortfolioValueMorning=self.strategiesData['bilstmWindowRobMMT1T2Legacy_25_1_2023'].TotalPortfolioValue[-2],
+									   totalPortfolioValueAfternoon=self.strategiesData['bilstmWindowRobMMT1T2Legacy_25_1_2023'].TotalPortfolioValue[-1],
+									   meanPortfolioValue=self.strategiesData['bilstmWindowRobMMT1T2Legacy_25_1_2023'].MPV[-2],
+									   caPortfolioValue=self.strategiesData['ca_25_1_2022'].TotalPortfolioValue[-2],
+									   caMeanPortfolioValue=self.strategiesData['ca_25_1_2022'].MPV[-2],
+									   bahPortfolioValue=self.strategiesData['bah_25_1_2022'].TotalPortfolioValue[-2],
+									   bahMeanPortfolioValue=self.strategiesData['bah_25_1_2022'].MPV[-2],
+									   randomPortfolioValue=self.strategiesData['random_25_1_2022'].TotalPortfolioValue[-2],
+									   randomMeanPortfolioValue=self.strategiesData['random_25_1_2022'].MPV[-2],
+									   idlePortfolioValue=self.strategiesData['idle_25_1_2022'].TotalPortfolioValue[-2],
+									   idleMeanPortfolioValue=self.strategiesData['idle_25_1_2022'].MPV[-2],
+									   todayDate=self.stockData.index[-1].to_pydatetime().strftime('%d-%m-%Y'),
+									   todayDecision=todayDecision,
+									   graph_url=graph_url)
+		return renderedHTML
 
 	def renderLongDailyDigest(self) -> str:
 		"""
@@ -45,3 +88,24 @@ class Renderer:
 		:return: html string
 		"""
 		return ''
+
+	def generateHTMLImageMPV(self) -> str:
+		latestMorning = self.strategiesData[list(self.strategiesData.keys())[0]].index[-2]
+		df = pd.DataFrame()
+
+		for key in self.strategiesData:
+			data = self.strategiesData[key]
+			data = data.query('index == @latestMorning')
+			df = pd.concat([df, data], ignore_index=True)
+
+		fig = go.Figure()
+		fig.add_trace(go.Bar(x=df['investorStrategy'], y=df['MPV'] - 10000))
+		fig.update_layout(title='Mean Portfolio Value Comparison (10,000$ offset)')
+		url = py.plot(fig, filename='example', auto_open=False)
+		return url
+
+
+if __name__ == '__main__':
+	todayDate = datetime.now(pytz.timezone('America/New_York'))
+	rend = Renderer(todayDate)
+	renderedHTML = rend.renderShortDailyDigest()
