35bdda6f97375fca67ea114b2e199ec59316e4f6
CJLlorente99 <cj.llorente@alumnos.upm.es>
2023-02-01 23:44:57 +0100

    Changes for running simulation to show in presentation

--- RF_DT/investorRandomForestClassifier.py
+++ RF_DT/investorRandomForestClassifier.py
@@ -15,9 +15,8 @@ from sklearn.ensemble import RandomForestClassifier
 
 class InvestorRandomForestClassifier(Investor):
 
-	def __init__(self, initialInvestment, var):
+	def __init__(self, initialInvestment):
 		super().__init__(initialInvestment)
-		self.var = var
 
 	def returnBrokerUpdate(self, moneyInvestedToday, data) -> pd.DataFrame:
 		return pd.DataFrame(
@@ -26,10 +25,7 @@ class InvestorRandomForestClassifier(Investor):
 			index=[0])
 
 	def possiblyInvestMorning(self, data):
-		if self.var == 1:
-			model = self.createAndTrainModel0(data['df'])
-		elif self.var == 2:
-			model = self.createAndTrainModel1(data['df'])
+		model = self.createAndTrainModel(data['df'])
 		res = self.calculateInputsMorning(data['df'])
 		self.perToInvest = model.predict([res.to_numpy()[-1]])[0]
 
@@ -80,7 +76,7 @@ class InvestorRandomForestClassifier(Investor):
 						self.record.index[-1].strftime("%d_%m_%Y") + ").png", scale=6, width=1080, height=1080)
 		# fig.show()
 
-	def createAndTrainModel0(self, df: pd.DataFrame):
+	def createAndTrainModel(self, df: pd.DataFrame):
 		data = df.copy()
 		data['Open'] = data['Open'].shift(-1)
 		data = data[:-2] # last is nan and second last is the input for prediction
@@ -182,112 +178,6 @@ class InvestorRandomForestClassifier(Investor):
 		model = RandomForestClassifier(bootstrap=False, criterion='entropy', max_depth=4, max_features='sqrt',
 									   min_samples_leaf=3,
 									   min_samples_split=4, n_estimators=108)
-		model.fit(X, y_target)
-
-		return model
-
-	def createAndTrainModel1(self, df: pd.DataFrame):
-		data = df.copy()
-		data['Open'] = data['Open'].shift(-1)
-		data = data[:-2] # last is nan and second last is the input for prediction
-
-		res = pd.DataFrame()
-		# Intraday return (target)
-		res['Target'] =  data['Open'] - data['Open'].shift()
-		y_target = np.asarray([1 if res.Target[i] > 0 else 0 for i in range(len(res))]).reshape(-1, 1)
-
-		# Return_interday
-		res['Return_interday'] = np.log(data['Open']) - np.log(data['Close'])
-
-		# bb_pband_w3_stdDev1.774447792366109
-		params = BBInvestorParams(3, 1.775)
-		res['bb_pband_w3_stdDev1.774447792366109'] = bollingerBands(data['Close'], params)['pband']
-
-		# Return_open
-		res['Return_open'] = np.log(data['Open']) - np.log(data['Open'].shift())
-
-		# adx_pos_w6
-		params = ADXInvestorParams(6)
-		res['adx_pos_w6'] = averageDirectionalMovementIndex(data['High'], data['Low'], data['Close'], params)['adx_pos']
-
-		# adx_pos_w42
-		params = ADXInvestorParams(42)
-		res['adx_pos_w42'] = averageDirectionalMovementIndex(data['High'], data['Low'], data['Close'], params)[
-			'adx_pos']
-
-		# Volume
-		res['Volume'] = data['Volume']
-
-		# adx_neg_w1
-		params = ADXInvestorParams(1)
-		res['adx_neg_w1'] = averageDirectionalMovementIndex(data['High'], data['Low'], data['Close'], params)['adx_neg']
-
-		# Return_intraday
-		res['Return_intraday'] = np.log(data['Close']) - np.log(data['Open'])
-
-		# stochRsi_k_w47_s143_s212
-		params = StochasticRSIInvestorParams(47, 43, 12)
-		res['stochRsi_k_w47_s143_s212'] = stochasticRSI(data['Close'], params)['k']
-
-		# stochRsi_d_w9_s144_s246
-		params = StochasticRSIInvestorParams(9, 44, 46)
-		res['stochRsi_d_w9_s144_s246'] = stochasticRSI(data['Close'], params)['d']
-
-		# stochRsi_d_w4_s16_s233
-		params = StochasticRSIInvestorParams(4, 6, 33)
-		res['stochRsi_d_w4_s16_s233'] = stochasticRSI(data['Close'], params)['d']
-
-		# adx_w10
-		params = ADXInvestorParams(10)
-		res['adx_w10'] = averageDirectionalMovementIndex(data['High'], data['Low'], data['Close'], params)['adx']
-
-		# bb_pband_w7_stdDev1.4065306043590475
-		params = BBInvestorParams(7, 1.407)
-		res['bb_pband_w7_stdDev1.4065306043590475'] = bollingerBands(data['Close'], params)['pband']
-
-		# bb_pband_w13_stdDev1.7961852973078898
-		params = BBInvestorParams(13, 1.796)
-		res['bb_pband_w13_stdDev1.7961852973078898'] = bollingerBands(data['Close'], params)['pband']
-
-		# adx_w18
-		params = ADXInvestorParams(18)
-		res['adx_w18'] = averageDirectionalMovementIndex(data['High'], data['Low'], data['Close'], params)['adx']
-
-		# stochRsi_k_w4_s16_s233
-		params = StochasticRSIInvestorParams(4, 6, 33)
-		res['stochRsi_k_w4_s16_s233'] = stochasticRSI(data['Close'], params)['k']
-
-		# adx_neg_w25
-		params = ADXInvestorParams(25)
-		res['adx_neg_w25'] = averageDirectionalMovementIndex(data['High'], data['Low'], data['Close'], params)[
-			'adx_neg']
-
-		# stochRsi_d_w12_s125_s25
-		params = StochasticRSIInvestorParams(12, 25, 5)
-		res['stochRsi_d_w12_s125_s25'] = stochasticRSI(data['Close'], params)['d']
-
-		# macd_difffW5_sW39_signal14
-		params = MACDInvestorParams(5, 39, 14)
-		res['macd_difffW5_sW39_signal14'] = movingAverageConvergenceDivergence(data['Close'], params)['diff']
-
-		# stochRsi_k_w29_s18_s219
-		params = StochasticRSIInvestorParams(29, 8, 19)
-		res['stochRsi_k_w29_s18_s219'] = stochasticRSI(data['Close'], params)['k']
-
-		res.dropna(inplace=True)
-		y_target = y_target[1:]
-		X = res.drop(['Target'], axis=1)
-
-		# include t-window data points as additional features
-		inp = X.columns.values.tolist()
-		# window mabye 0 to 1,2,3
-		window = 0
-		X = data_shift(X, window, inp)
-		X = np.asarray(X)
-
-		model = RandomForestClassifier(bootstrap=False, criterion='entropy', max_depth=4, max_features='auto',
-									   min_samples_leaf=6,
-									   min_samples_split=2, n_estimators=753)
 		model.fit(X, y_target)
 
 		return model
--- mainComparing.py
+++ mainComparing.py
@@ -2,14 +2,10 @@ import datetime
 import os.path
 import pandas as pd
 from classes.dataClass import DataGetter
-from LSTM.investorBiLSTMWindowMinMaxScaler import InvestorBiLSTMWindowMinMaxT1, InvestorBiLSTMWindowMinMaxT2, InvestorBiLSTMWindowMinMaxT1T2
-from LSTM.investorBiLSTMWindowMinMaxScalerLegacy import InvestorBiLSTMWindowMinMaxT1Legacy, InvestorBiLSTMWindowMinMaxT2Legacy, InvestorBiLSTMWindowMinMaxT1T2Legacy
-from LSTM.investorBiLSTMWindowRobustMinMaxScaler import InvestorBiLSTMWindowRobustMinMaxT1, InvestorBiLSTMWindowRobustMinMaxT2, InvestorBiLSTMWindowRobustMinMaxT1T2
-from LSTM.investorBiLSTMWindowRobustMinMaxScalerLegacy import InvestorBiLSTMWindowRobustMinMaxT1Legacy, InvestorBiLSTMWindowRobustMinMaxT2Legacy, InvestorBiLSTMWindowRobustMinMaxT1T2Legacy
-from LSTM.investorLSTMWindowMinMaxScaler import InvestorLSTMWindowMinMaxT1, InvestorLSTMWindowMinMaxT2, InvestorLSTMWindowMinMaxT1T2
-from LSTM.investorLSTMWindowMinMaxScalerLegacy import InvestorLSTMWindowMinMaxT1Legacy, InvestorLSTMWindowMinMaxT2Legacy, InvestorLSTMWindowMinMaxT1T2Legacy
-from LSTM.investorLSTMWindowRobustMinMaxScaler import InvestorLSTMWindowRobustMinMaxT1, InvestorLSTMWindowRobustMinMaxT2, InvestorLSTMWindowRobustMinMaxT1T2
-from LSTM.investorLSTMWindowRobustMinMaxScalerLegacy import InvestorLSTMWindowRobustMinMaxT1Legacy, InvestorLSTMWindowRobustMinMaxT2Legacy, InvestorLSTMWindowRobustMinMaxT1T2Legacy
+from LSTM.investorBiLSTMWindowRobustMinMaxScalerLegacy import InvestorBiLSTMWindowRobustMinMaxT1T2Legacy
+from LSTM.investorLSTMWindowRobustMinMaxScalerLegacy import InvestorLSTMWindowRobustMinMaxT2Legacy
+from RF_DT.investorRandomForestClassifier import InvestorRandomForestClassifier
+from RF_DT.investorXGB import InvestorXGB
 from Benchmarks.randomBenchmark import InvestorRandom
 from Benchmarks.bia import InvestorBIA
 from Benchmarks.wia import InvestorWIA
@@ -22,10 +18,10 @@ from classes.experimentManager import ExperimentManager
 
 def main():
     # Create DataGetter instance
-    dataGetter = DataGetter('2022-12-01', '2022-12-30')
+    dataGetter = DataGetter('2019-12-01', '2022-12-30')
 
     # Run various experiments
-    numExperiments = 4
+    numExperiments = 30
     nDays = 10
     dfTestCriteria = pd.DataFrame()
 
@@ -40,40 +36,11 @@ def main():
         df = dataGetter.getPastData()
 
         """
-        BiLSTM RobMinMax
-        """
-
-        # Create investor based on window forecasting (open_t - open_t+2)
-        investorBiLSTMWindowRobMMT1 = InvestorBiLSTMWindowRobustMinMaxT1(10000, 1)
-        experimentManager.addStrategy(investorBiLSTMWindowRobMMT1, "bilstmWindowRobMMT1",
-                                      [experimentManager.createTIInput("df")], True)
-
-        # Create investor based on window forecasting (open_t - open_t+3)
-        investorBiLSTMWindowRobMMT2 = InvestorBiLSTMWindowRobustMinMaxT2(10000, 1)
-        experimentManager.addStrategy(investorBiLSTMWindowRobMMT2, "bilstmWindowRobMMT2",
-                                      [experimentManager.createTIInput("df")], True)
-
-        # Create investor based on window forecasting (open_t - open_t+2)
-        investorBiLSTMWindowRobMMT1T2 = InvestorBiLSTMWindowRobustMinMaxT1T2(10000, 1)
-        experimentManager.addStrategy(investorBiLSTMWindowRobMMT1T2, "bilstmWindowRobMMT1T2",
-                                      [experimentManager.createTIInput("df")], True)
-
-        """
         BiLSTM RobMinMax Legacy
         """
 
         # Create investor based on window forecasting (open_t - open_t+2)
-        investorBiLSTMWindowRobMMT1Legacy = InvestorBiLSTMWindowRobustMinMaxT1Legacy(10000, 1)
-        experimentManager.addStrategy(investorBiLSTMWindowRobMMT1Legacy, "bilstmWindowRobMMT1Legacy",
-                                      [experimentManager.createTIInput("df")], True)
-
-        # Create investor based on window forecasting (open_t - open_t+3)
-        investorBiLSTMWindowRobMMT2Legacy = InvestorBiLSTMWindowRobustMinMaxT2Legacy(10000, 1)
-        experimentManager.addStrategy(investorBiLSTMWindowRobMMT2Legacy, "bilstmWindowRobMMT2Legacy",
-                                      [experimentManager.createTIInput("df")], True)
-
-        # Create investor based on window forecasting (open_t - open_t+2)
-        investorBiLSTMWindowRobMMT1T2Legacy = InvestorBiLSTMWindowRobustMinMaxT1T2Legacy(10000, 1)
+        investorBiLSTMWindowRobMMT1T2Legacy = InvestorBiLSTMWindowRobustMinMaxT1T2Legacy(10000, 5)
         experimentManager.addStrategy(investorBiLSTMWindowRobMMT1T2Legacy, "bilstmWindowRobMMT1T2Legacy",
                                       [experimentManager.createTIInput("df")], True)
 
@@ -81,20 +48,27 @@ def main():
         LSTM RobMinMax Legacy
         """
 
-        # Create investor based on window forecasting (open_t - open_t+2)
-        investorLSTMWindowRobMMT1Legacy = InvestorLSTMWindowRobustMinMaxT1Legacy(10000, 1)
-        experimentManager.addStrategy(investorLSTMWindowRobMMT1Legacy, "lstmWindowRobMMT1Legacy",
-                                      [experimentManager.createTIInput("df")], True)
-
         # Create investor based on window forecasting (open_t - open_t+3)
-        investorLSTMWindowRobMMT2Legacy = InvestorLSTMWindowRobustMinMaxT2Legacy(10000, 1)
+        investorLSTMWindowRobMMT2Legacy = InvestorLSTMWindowRobustMinMaxT2Legacy(10000, 5)
         experimentManager.addStrategy(investorLSTMWindowRobMMT2Legacy, "lstmWindowRobMMT2Legacy",
                                       [experimentManager.createTIInput("df")], True)
 
-        # Create investor based on window forecasting (open_t - open_t+2)
-        investorLSTMWindowRobMMT1T2Legacy = InvestorLSTMWindowRobustMinMaxT1T2Legacy(10000, 1)
-        experimentManager.addStrategy(investorLSTMWindowRobMMT1T2Legacy, "lstmWindowRobMMT1T2Legacy",
+        """
+        Random Forest
+        """
+
+        investorRandomForest = InvestorRandomForestClassifier(10000)
+        experimentManager.addStrategy(investorRandomForest, "randomForest",
                                       [experimentManager.createTIInput("df")], True)
+
+        """
+        XGBoost
+        """
+
+        investorXGBoost = InvestorXGB(10000)
+        experimentManager.addStrategy(investorXGBoost, "xgBoost",
+                                      [experimentManager.createTIInput("df")], True)
+
 
         # Create investor Random
         investorRandom = InvestorRandom(10000)
